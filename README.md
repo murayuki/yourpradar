af76c8f5e18d736b4e7e6dcdb474d7b0a6953571
Yozz <50157385+Yozz001@users.noreply.github.com>
2019-12-06 12:40:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# esx_yourpradar
+ My version of esx_policeradar - i found this a while back and cannot find the orginal authors name anymore if anyone can please let me know so i can leave credits.
---
+++ __resource.lua
@@ -0,0 +1,18 @@
+files { 
+	"html/radar.css",
+    "html/radar.js",   
+    "html/jquery.js", 
+    "html/radar.png",  
+    "html/power_on.png",
+    "html/power_off.png",
+	"html/radar.html"
+}
+
+ui_page "html/radar.html"
+
+server_scripts {    
+}
+
+client_script{
+    "client.lua"
+}
